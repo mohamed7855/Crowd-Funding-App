ef03909b10c847caa0876150ad8c11961218bb51
MohamedWahba10 <Mohamed.wahba9910@gmail.com>
2024-02-13 19:54:14 +0200

    main page

--- fundProject/views.py
+++ fundProject/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def mainPage(request):
-    return  render(request,'index.html')
+     # images = ['iot.jpg', 'os.jpg', 'php.jpg'] 
+     return  render(request,'index.html')
+
     # return HttpResponse("Hello omara World")
+
+# views.py
+
+
+# def slider_view(request):
+#     # List your image filenames here
+#     return render(request, 'your_app/slider.html')
