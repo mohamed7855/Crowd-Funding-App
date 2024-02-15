c0f65ec3431bb6e8259f6c7d566938cf72429e5c
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-02-15 11:08:46 +0200

    adding Project views (List, details, add and report comment, cancel) and Urls

--- fundProject/urls.py
+++ fundProject/urls.py
@@ -4,11 +4,19 @@ from .views import *
 
 
 urlpatterns = [
-    path('addCategory/', addCategory, name='addCategory'),
-    path('delete-category/', deleteCategory, name='deleteCategory'),
-    path('', mainPage, name='mainPage'),
-    path('addProject/', addProject, name='addProject'),
-    path('detailProject/',detailProject, name='detailProject')
+    
+        path('addCategory/', addCategory, name='addCategory'),
+        path('delete-category/', deleteCategory, name='deleteCategory'),
+        path('', mainPage, name='mainPage'),
+        path('addProject/', addProject, name='addProject'),
+        path('detailProject/',detailProject, name='detailProject'),
+        # path('projectList/', projectList),
+        path('projectList/<int:id>/', projectList),
+        path('projectDetails/<int:id>/', projectDetails),
+        path('addCommentOnProject/<int:id>/', addCommentOnProject),
+        path('reportComment/<int:id>/', reportComment),
+        path('cancelProject/<int:id>/', cancelProject),
+    
 ]
  
 
