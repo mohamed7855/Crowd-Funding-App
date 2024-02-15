53fdbe75fc2b98add712dcc9f428605326cc7252
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-02-15 11:15:00 +0200

    Recommitting Urls after resolve conflict

--- fundProject/urls.py
+++ fundProject/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('delete-category/', deleteCategory, name='deleteCategory'),
     path('', mainPage, name='mainPage'),
     path('addProject/', addProject, name='addProject'),
-    path('detailProject/',detailProject, name='detailProject')
+    path('detailProject/',detailProject, name='detailProject'),
+    # path('projectList/', projectList),
+    path('projectList/<int:id>/', projectList),
+    path('projectDetails/<int:id>/', projectDetails),
+    path('addCommentOnProject/<int:id>/', addCommentOnProject),
+    path('reportComment/<int:id>/', reportComment),
+    path('cancelProject/<int:id>/', cancelProject),
 ]
 
