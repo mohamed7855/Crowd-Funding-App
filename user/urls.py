0ea2825eb29d508980677354ab5d0356bc9fb62c
Merna Bassem <mernabassem21@gmail.com>
2024-02-15 14:13:38 +0200

    Adding page allUser

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('addUser/', addUser, name='addUser'),
+    path('', allUser, name='allUser'),
 
 ]
  
--- user/views.py
+++ user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def addUser(request):
      return  render(request,'addUser.html')
 
+def allUser(request):
+     return  render(request,'allUser.html')
+
