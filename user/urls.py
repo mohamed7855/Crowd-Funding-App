742166a83ea8044e4092cb79e0044fc768b74684
MohamedWahba10 <Mohamed.wahba9910@gmail.com>
2024-02-15 21:33:39 +0200

    fix error

--- user/urls.py
+++ user/urls.py
@@ -4,18 +4,10 @@ from .views import *
 
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('updateUser/', updateUser, name='updateUser'),
     path('insertuser/', insertuser, name='insertuser'),
     # path('delete-category/', deleteCategory, name='deleteCategory'),
     
 ]
  
-
-=======
-    path('addUser/', addUser, name='addUser'),
-    path('', allUser, name='allUser'),
-
-]
- 
->>>>>>> d0f8258be0e0d0476c7af31a2496f189b1ca276f
