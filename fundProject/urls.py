c1200a3b70068130fcbefe1623b0ca9722cd4179
Merna Bassem <mernabassem21@gmail.com>
2024-02-15 09:48:39 +0200

    Adding forms.py and page html by dorm

--- fundProject/urls.py
+++ fundProject/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', mainPage, name='mainPage'),
     path('addProject/', addProject, name='addProject'),
+    path('addProjectForm/', addProjectForm, name='addProjectForm'),
     path('detailProject/',detailProject, name='detailProject')
 
 ]
--- fundProject/views.py
+++ fundProject/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render, redirect , reverse,HttpResponse
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
 
 def mainPage(request):
      return  render(request,'index.html')
@@ -13,4 +13,15 @@ def detailProject(request):
      return  render(request,'fundProject/detailProject.html')
 
 
+from .forms import ProjectForm
+
+def addProjectForm(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("detailProject"))
+    else:
+        form = ProjectForm()
+    return render(request, 'fundProject/addProjectForm.html', {'form': form})
 
